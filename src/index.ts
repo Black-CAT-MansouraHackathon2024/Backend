ed95c7876834f7f295d42f2e5b297c96b481f86f
Basma-90 <basmasabry33333@gmail.com>
2024-12-02 10:49:08 +0200

    Add health check endpoint to confirm server is running

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,9 @@ app.use(expressRateLimit({
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
 app.use('/api/users', userRouter);
 app.use('/api/ideas', ideaRouter);
 app.use('/api/prototypes', prototypeRouter);
