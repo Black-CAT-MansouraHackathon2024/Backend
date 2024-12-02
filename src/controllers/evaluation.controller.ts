ead0393a7483700250b31312a1600f0bbc366e9e
Basma-90 <basmasabry33333@gmail.com>
2024-12-02 10:26:56 +0200

    Refactor evaluation controller to improve user authorization checks and ensure asynchronous handling of evaluation data

--- src/controllers/evaluation.controller.ts
+++ src/controllers/evaluation.controller.ts
@@ -7,15 +7,18 @@ import {
     getIdeaEvaluatorEvaluationById,
 } from '../services/evaluation.services';
 import { getUserIdea } from '../services/idea.services';
+import dotenv from 'dotenv';
+import { dot } from 'node:test/reporters';
+dotenv.config();
 
 
 export const createEvaluationController = async (req: Request, res: Response, next: NextFunction) => {
     try{
         const user= req.body.user;
-        if(user.role !== 'investor' || user.role !== 'consultant'){ 
+        if(user.role !== 'investor'){ 
             throw new Error('Unauthorized');
         }
-        const evaluatedData = giveEvaluation(req.body);
+        const evaluatedData = await giveEvaluation(req.body);
         res.status(201).send(evaluatedData);
     }
     catch(err){
@@ -26,15 +29,18 @@ export const createEvaluationController = async (req: Request, res: Response, ne
 export const getIdeaEvaluationController = async (req: Request, res: Response, next: NextFunction) => {
     try{
         const user = req.body.user;
-        const userIdea= getUserIdea(user._id);
-        if(user.role !== 'investor' || user.role !== 'consultant'){
+        console.log(req.params.id);
+        const userIdea= await getUserIdea(req.params.ideaId);
+        console.log(userIdea.toString());
+        console.log(user._id.toString());
+
+        if(user.role !== 'investor' ){
             throw new Error('Unauthorized');
         }
-        if(userIdea !==user._id){
+        if(userIdea.toString() !==user._id.toString()){
             throw new Error('Unauthorized');
         }
-        const evaluationId = req.params.id;
-        const ideaEvaluation = getIdeaEvaluation(evaluationId);
+        const ideaEvaluation = await getIdeaEvaluation(req.params.ideaId);
         res.status(200).send(ideaEvaluation);
 
     }catch(err:any){
@@ -45,12 +51,12 @@ export const getIdeaEvaluationController = async (req: Request, res: Response, n
 
 export const getEvaluatorEvaluationController = async (req: Request, res: Response, next: NextFunction) => {
     try{
-        const evaluatorId = req.params.id;
+        const evaluatorId = req.params.evaluatorId;
         const userId = req.body.user._id;
-        if(evaluatorId !== userId){
+        if(evaluatorId.toString() !== userId.toString()){
             throw new Error('Unauthorized');
         }
-        const evaluatorEvaluation = getEvaluatorEvaluation(evaluatorId);
+        const evaluatorEvaluation = await getEvaluatorEvaluation(evaluatorId);
         res.status(200).send(evaluatorEvaluation);
     }catch(err:any){
         next(err);
@@ -61,16 +67,17 @@ export const getEvaluatorEvaluationController = async (req: Request, res: Respon
 export const getIdeaEvaluatorEvaluationController = async (req: Request, res: Response, next: NextFunction) => {//idea owner ,investors or consultants
     try{
         const user = req.body.user;
-        const userIdea= getUserIdea(user._id);
-        if(userIdea !==user._id){
+        const userIdea= await getUserIdea(req.params.ideaId);
+        
+        if(userIdea.toString() !==user._id.toString()){
             throw new Error('Unauthorized');
         }
-        if(user.role !== 'investor' || user.role !== 'consultant'){
+        if(user.role !== 'investor' ){
             throw new Error('Unauthorized');
         }
         const ideaId = req.params.ideaId;
         const evaluatorId = req.params.evaluatorId;
-        const ideaEvaluatorEvaluation = getIdeaEvaluatorEvaluation(ideaId, evaluatorId);
+        const ideaEvaluatorEvaluation = await getIdeaEvaluatorEvaluation(ideaId, evaluatorId);
         res.status(200).send(ideaEvaluatorEvaluation);
     }catch(err:any){
         next(err);
@@ -81,17 +88,17 @@ export const getIdeaEvaluatorEvaluationController = async (req: Request, res: Re
 export const getIdeaEvaluatorEvaluationByIdController = async (req: Request, res: Response, next: NextFunction) => {
     try{
         const user = req.body.user;
-        const userIdea= getUserIdea(user._id);
-        if(userIdea !==user._id){
+        const userIdea= await getUserIdea(req.params.ideaId);
+        if(userIdea.toString() !==user._id.toString()){
             throw new Error('Unauthorized');
         }
-        if(user.role !== 'investor' || user.role !== 'consultant'){
+        if(user.role !== 'investor'){
             throw new Error('Unauthorized');
         }
         const ideaId = req.params.ideaId;
         const evaluatorId = req.params.evaluatorId;
-        const evaluationId = req.params.id;
-        const ideaEvaluatorEvaluation = getIdeaEvaluatorEvaluationById(ideaId, evaluatorId, evaluationId);
+        const evaluationId = req.params.evaluationId;
+        const ideaEvaluatorEvaluation = await getIdeaEvaluatorEvaluationById(ideaId, evaluatorId, evaluationId);
         res.status(200).send(ideaEvaluatorEvaluation);
     }catch(err:any){
         next(err);
